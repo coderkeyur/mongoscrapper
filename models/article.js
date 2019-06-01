b6426ce8ef03a9a75363baeb587f7d61773085fe
KP <cle.keyur@gmail.com>
2019-05-31 20:08:04 -0400

    added handlebars folders and module updates

--- models/article.js
+++ models/article.js
@@ -15,6 +15,10 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    note: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+
+var app = express();
