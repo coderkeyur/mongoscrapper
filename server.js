d573a8015d2b3c2c30ac5a5d1333d8c3ff4f1159
KP <cle.keyur@gmail.com>
2019-05-31 20:39:36 -0400

    running server file

--- server.js
+++ server.js
@@ -3,8 +3,34 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
+var request = require ("request");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+
+app.listen(PORT, function() {
+    console.log("App is running on port " + PORT + "!");
+});
