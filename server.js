90e79d148709906c8e7cdaf38f7f2874d4fcaa18
KP <cle.keyur@gmail.com>
2019-06-04 23:49:29 -0400

    updates to project

--- server.js
+++ server.js
@@ -36,72 +36,64 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
     var found;
     var titleArr = [];
-      db.Article.find({})
-        .then(function(dbArticle) {
-          for (var j=0; j<dbArticle.length;j++) {
-            titleArr.push(dbArticle[j].title)
-          }
-          console.log(titleArr);
-      request("https://universe.byu.edu/", function(error, response, html) {
-      if (!error && response.statusCode == 200) {
-          // console.log(html);
-        }
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(html, {
-        xml: {
-          normalizeWhitespace: true,
-        }
-      })
-      $("body h3").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(element).children("a").text();
-        found = titleArr.includes(result.title);
-        result.link = $(element).children("a").attr("href");
-        result.excerpt = $(element).parent().children(".td-excerpt").text().trim();
-        if (!found && result.title && result.link){
-          results.push(result);
-       }
-      });
-        res.render("scrape", {
-        articles: results
-      });
-    })
-  });
-  });
+    db.Article.find({})
+        .then(function (dbArticle) {
+            for (var j = 0; j < dbArticle.length; j++) {
+                titleArr.push(dbArticle[j].title)
+            }
+            console.log(titleArr);
+            request("https://universe.byu.edu/", function (error, response, html) {
+                if (!error && response.statusCode == 200) {
+                }
+                var $ = cheerio.load(html, {
+                    xml: {
+                        normalizeWhitespace: true,
+                    }
+                })
+                $("body h3").each(function (i, element) {
+                    
+                    var result = {};
+                    result.title = $(element).children("a").text();
+                    found = titleArr.includes(result.title);
+                    result.link = $(element).children("a").attr("href");
+                    result.excerpt = $(element).parent().children(".td-excerpt").text().trim();
+                    if (!found && result.title && result.link) {
+                        results.push(result);
+                    }
+                });
+                res.render("scrape", {
+                    articles: results
+                });
+            })
+        });
+});
 
-// app.get("/scrape", function (req, res) {
-//     axios.get("http://www.echojs.com/").then(function (response) {
-//         var $ = cheerio.load(response.data);
+app.get("/saved", function (req, res) {
 
-//         $("article h3").each(function (i, element) {
-//             var result = {};
+    db.Article.find({})
+        .then(function (dbArticle) {
+            console.log(dbArticle);
+            res.render("saved", {
+                saved: dbArticle
+            });
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
-//             result.title = $(this)
-//                 .children("a")
-//                 .text();
-//             result.link = $(this)
-//                 .children("a")
-//                 .attr("href");
 
-//             db.Article
-//                 .create(result)
-//                 .then(function (dbArticle) {
-//                     res.send("Scrape Complete");
-//                 })
-//                 .catch(function (err) {
-//                     res.json(err)
-//                 });
-//         });
-//     });
-// });
-
-app.get("/articles", function (req, res) {
-
+app.post("/api/saved", function (req, res) {
+    db.Article.create(req.body)
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 app.get("/articles/:id", function (req, res) {
@@ -112,8 +104,32 @@ app.post("/articles/:id", function (req, res) {
 
 });
 
+app.get("/articles/:id", function (req, res) {
+    console.log(req.params.id);
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            console.log(dbArticle);
+            if (dbArticle) {
+                res.render("articles", {
+                    data: dbArticle
+                });
+            }
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 
+app.delete("/saved/:id", function (req, res) {
+    db.Article.deleteOne({ _id: req.params.id })
+        .then(function (removed) {
+            res.json(removed);
+        }).catch(function (err, removed) {
+            res.json(err);
+        });
+});
 
 app.listen(PORT, function () {
     console.log("App is running on port " + PORT + "!");
